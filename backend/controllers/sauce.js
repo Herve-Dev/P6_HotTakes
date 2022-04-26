da9c43e1e769fb8b833673c223e87a9adf174241
Doc_Dev <madinina972represent@gmail.com>
2022-04-26 21:36:28 +0200

    Update fs Unlink

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,67 +1,79 @@
-const { json } = require('body-parser');
-const Sauce = require('../models/Sauce');
-
+const { json } = require("body-parser");
+const Sauce = require("../models/Sauce");
+const fs = require("fs");
 
 exports.createSauce = (req, res, next) => {
   const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
-    const sauce = new Sauce({
-      ...sauceObject, 
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-      likes: 0,
-      dislikes: 0,
-      usersLiked: "",
-      usersDisliked: "",
-    });
-    //console.log(sauceObject);
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Sauce enregistré !'}))
-      .catch(error => res.status(400).json({ error }));
-}
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get("host")}/images/${
+      req.file.filename
+    }`,
+    likes: 0,
+    dislikes: 0,
+    usersLiked: "",
+    usersDisliked: "",
+  });
+  //console.log(sauceObject);
+  sauce
+    .save()
+    .then(() => res.status(201).json({ message: "Sauce enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.modifySauce = (req, res, next) => {
-    const sauceObject = req.file ?
-    {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body };
-    Sauce.updateOne({ _id: req.params.id }, {...sauceObject, _id: req.params.id})
-        .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
-        .catch(error => res.status(400).json({ error }))
-}
+  const sauceObject = req.file
+    ? {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get("host")}/images/${
+          req.file.filename
+        }`,
+      }
+    : { ...req.body };
+  Sauce.updateOne(
+    { _id: req.params.id },
+    { ...sauceObject, _id: req.params.id }
+  )
+    .then(() => res.status(200).json({ message: "Sauce modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.deleteSauce = (req, res, next) => {
-    Sauce.findOne({_id: req.params.id})
-      .then(
-        (sauce) => {
-          if (!sauce) {
-            return res.status(404).json({
-              error: new Error ('Objet non trouvé !')
-            });
-          }
-          if (sauce.userId !== req.auth.userId) {
-            return res.status(400).json({
-              error: new Error('Requete non autorisée !')
-            })
-          }
-          Sauce.deleteOne({_id: req.params.id})
-            .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-            .catch(error => res.status(400).json({ error }))
-        }
-      ) 
-}
+  
+
+  Sauce.findOne({ _id: req.params.id }).then((sauce) => {
+    if (!sauce) {
+      return res.status(404).json({
+        error: new Error("Objet non trouvé !"),
+      });
+    }
+    if (sauce.userId !== req.auth.userId) {
+      return res.status(400).json({
+        error: new Error("Requete non autorisée !"),
+      });
+    }
+
+    const fileName = sauce.imageUrl.split("/images/")[1];
+    fs.unlink(`images/${fileName}`, () => {
+      Sauce.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: "Sauce supprimé !" }))
+        .catch((error) => res.status(400).json({ error }));
+    });
+  });
+};
 
 exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-      .then(sauce => res.status(200).json(sauce))
-      .catch(error => res.status(400).json({ error }));
-}
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => res.status(200).json(sauce))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.getAllSauce = (req, res, next) => {
-    Sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
-}
+  Sauce.find()
+    .then((sauces) => res.status(200).json(sauces))
+    .catch((error) => res.status(400).json({ error }));
+};
 
 exports.likeOrNotLike = (req, res, next) => {
   if (req.body.like === 1) {
